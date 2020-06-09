f8fea72d6996387e927834a7bf6e8b5b3a857420
Daniel Reszka <dannn.r@gmail.com>
2020-06-09 19:55:54 +0200

    DB: setup db with docker-compose & postgres

--- .env.example.js
+++ .env.example.js
@@ -1,16 +1,25 @@
 if (!process.env.NODE_ENV) process.env.NODE_ENV = "development";
+const { NODE_ENV } = process.env;
 
 const PORT_BASE = 3000;
 
 const env = {
   PORT_UI: PORT_BASE,
   PORT_API: PORT_BASE + 1000,
+  PORT_PG_ADMINER: 8000,
+  PORT_PG: 8001,
 
   API_AUTH_COOKIE_KEY1_SECRET: "secret-hgvkgvsdjbad9876svdhkasq",
   API_AUTH_COOKIE_KEY2_SECRET: "secret-870oh3beucgTTIFCKJuihoew",
   API_AUTH_COOKIE_MAX_AGE_MINUTES: 60,
 
+  PG_DB: `proto_${NODE_ENV}`,
+  PG_PASSWORD: "proto",
+  PG_USER: "proto",
+
   SECURE: false,
 };
+
+env.PG_URL = `postgresql://${env.PG_USER}:${env.PG_PASSWORD}@localhost:${env.PORT_PG}/${env.PG_DB}?schema=public`;
 
 module.exports = env;
