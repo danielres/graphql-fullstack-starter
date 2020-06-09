6fece8f33db0b7b903cfc0500bfdda71d9737fb9
Daniel Reszka <dannn.r@gmail.com>
2020-06-09 19:48:13 +0200

    API: graphql: add User, add placeholders for signin + signup

--- api/src/schema/resolvers.ts
+++ api/src/schema/resolvers.ts
@@ -3,4 +3,24 @@ export default {
     hello: (_: any, __: any, ctx: any) =>
       `Hello from resolver! Views: ${ctx.req.session.views}`,
   },
+
+  Mutation: {
+    signin: (
+      _: any,
+      args: { input: { email: string; password: string } },
+      ctx: any
+    ) => {
+      const { input } = args;
+      return { id: "123", name: "u1", email: input.email };
+    },
+
+    signup: (
+      _: any,
+      args: { input: { email: string; name: string; password: string } },
+      ctx: any
+    ) => {
+      const { input } = args;
+      return true;
+    },
+  },
 };
--- api/src/schema/typeDefs.ts
+++ api/src/schema/typeDefs.ts
@@ -4,4 +4,37 @@ export default gql`
   type Query {
     hello: String!
   }
+
+  type Mutation {
+    signin(input: InputSignin!): User
+    signup(input: InputSignup!): Boolean
+  }
+
+  ### Scalars
+
+  scalar DateTime
+
+  ### Inputs
+
+  input InputSignin {
+    email: String!
+    password: String!
+  }
+
+  input InputSignup {
+    email: String!
+    name: String!
+    password: String!
+  }
+
+  ### Types
+
+  type User {
+    id: String!
+    name: String!
+    email: String!
+    password: String!
+    createdAt: DateTime!
+    updatedAt: DateTime
+  }
 `;
