a58e82b7170ffee6b5f4de0f03a9f3f611f68855
Daniel Reszka <dannn.r@gmail.com>
2020-06-10 14:57:37 +0200

    UI: add Layout using tailwind, remove demo contents

---
+++ ui/components/ui/Layout.js
@@ -0,0 +1,18 @@
+import React from "react";
+import classnames from "classnames";
+
+export default ({ children }) => {
+  return (
+    <div className="flex flex-col h-screen bg-gray-200">
+      <nav className="bg-black text-white mb-8">
+        <div className="container px-4 py-4 mx-auto">Nav</div>
+      </nav>
+      <div className="container mx-auto flex-grow flex flex-col">
+        <main className="px-4 py-4 flex-grow bg-white shadow-lg rounded">
+          Main
+        </main>
+        <footer className="px-4 py-4 text-gray-600">Footer</footer>
+      </div>
+    </div>
+  );
+};
