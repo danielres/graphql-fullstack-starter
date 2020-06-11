2df9aa7ca6712501ec7018832f76abd019c417b8
Daniel Reszka <dannn.r@gmail.com>
2020-06-11 20:20:36 +0200

    UI: Layout: nav: add links Home + Signout

--- ui/components/ui/Layout.js
+++ ui/components/ui/Layout.js
@@ -1,12 +1,16 @@
+import { useApolloClient, useMutation } from "@apollo/react-hooks";
 import React from "react";
-import classnames from "classnames";
+import * as queries from "../../queries";
 
 export default ({ children }) => {
   return (
     <div className="flex flex-col h-screen bg-gray-200">
       <nav className="bg-black text-white mb-8">
-        <div className="container px-4 py-4 mx-auto">Nav</div>
+        <div className="container px-4 py-4 mx-auto">
+          <NavContent />
+        </div>
       </nav>
+
       <div className="container mx-auto flex-grow flex flex-col">
         <main className="px-4 py-4 flex-grow bg-white shadow-lg rounded">
           {children}
@@ -16,3 +20,33 @@ export default ({ children }) => {
     </div>
   );
 };
+
+function NavContent() {
+  const [signout, { data, loading }] = useMutation(queries.SIGNOUT, {
+    refetchQueries: ["Me"],
+  });
+  const apolloClient = useApolloClient();
+
+  return (
+    <div className="flex justify-between">
+      <ul>
+        <li>Home</li>
+      </ul>
+
+      <ul>
+        <li>
+          <button
+            className="hover:underline text-gray-400"
+            onClick={async () => {
+              await signout();
+              apolloClient.stop();
+              location.reload();
+            }}
+          >
+            Signout
+          </button>
+        </li>
+      </ul>
+    </div>
+  );
+}
