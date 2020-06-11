2651806cffd835a05037df75abf1c48f0b159975
Daniel Reszka <dannn.r@gmail.com>
2020-06-11 22:23:07 +0200

    UI: FormSignin: handle async errors

--- ui/components/forms/FormSignin.js
+++ ui/components/forms/FormSignin.js
@@ -1,21 +1,26 @@
 import { useMutation } from "@apollo/react-hooks";
+import { useState } from "react";
 import { FormContext, useForm } from "react-hook-form";
 import * as queries from "../../queries";
 import Button from "../ui/Button";
+import AsyncError from "../ui/forms/AsyncError";
 import FormRow from "../ui/forms/FormRow";
 
 export default ({ onSuccess }) => {
   const { handleSubmit, ...rest } = useForm();
-  const [signin, { data, loading, error }] = useMutation(queries.SIGNIN);
 
-  const onSubmit = async (input) => {
-    await signin({ variables: { input } });
-    onSuccess();
-  };
+  const [signin, { data, loading, error }] = useMutation(queries.SIGNIN);
+  const [asyncError, setAsyncError] = useState();
+  const dismissAsyncError = () => setAsyncError();
+
+  const onSubmit = (input) =>
+    signin({ variables: { input } }).then(onSuccess).catch(setAsyncError);
 
   return (
     <FormContext {...rest}>
       <form onSubmit={handleSubmit(onSubmit)}>
+        <AsyncError asyncError={asyncError} dismiss={dismissAsyncError} />
+
         <FormRow
           label="Email"
           name="email"
