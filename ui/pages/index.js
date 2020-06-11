1b062c2fe48e110558ba7b0e1295276d026e3f7e
Daniel Reszka <dannn.r@gmail.com>
2020-06-11 20:14:23 +0200

    UI: pages: index: use demo query HELLO

--- ui/pages/index.js
+++ ui/pages/index.js
@@ -1,10 +1,16 @@
 import Head from "next/head";
 import Layout from "../components/ui/Layout";
+import * as queries from "../queries.js";
+import { useQuery } from "@apollo/react-hooks";
 
 export default () => {
+  const { data, loading } = useQuery(queries.HELLO);
+
+  if (!data) return "Loading...";
+
   return (
     <Layout>
-      <main>aaa</main>
+      <main>{data.hello}</main>
     </Layout>
   );
 };
