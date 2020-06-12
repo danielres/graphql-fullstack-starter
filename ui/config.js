f9680b0f0ffb9e219385f1330e2f74392c9abadd
Daniel Reszka <dannn.r@gmail.com>
2020-06-12 10:27:21 +0200

    UI: config: declare public routes

--- ui/config.js
+++ ui/config.js
@@ -1,11 +1,19 @@
 import validateConfig from "@danielres/validate-config";
 
-const checks = {};
-
 const config = {
   graphql: {
     URL: [process.env.NEXT_PUBLIC_GRAPHQL_URL, "string"],
   },
+
+  routes: {
+    PUBLIC: [["/signup"], "array"],
+  },
 };
 
-export default validateConfig(checks)(config);
+export default validateConfig(getChecks())(config);
+
+function getChecks() {
+  return {
+    array: [Array.isArray, (v) => v, "should be an array"],
+  };
+}
